0de2cb10233bba5193c0803dcf23b4a141c0a8ef
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2025-03-24 13:43:31 +0700

    Update data.js

--- public/assets/js/data.js
+++ public/assets/js/data.js
@@ -10,4 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     // หาปุ่มที่มี data-meet="2568"
+//     const button = document.querySelector('button[data-meet="2568"]');
+    
+//     if (button) {
+//         // เพิ่ม event listener เมื่อคลิก
+//         button.addEventListener('click', function() {
+//             window.location.href = '/meeting/';
+//         });
+//     }
+// });
+
       
