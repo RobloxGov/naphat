33891b18c816b57564cae760bd9c80cc12a0e773
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2025-03-24 12:23:52 +0700

    Update myscript.js

--- meeting/js/myscript.js
+++ meeting/js/myscript.js
@@ -2,12 +2,9 @@
     function formatInput(event) {
       let input = event.target.value;
       
-      // ลบอักขระที่ไม่ใช่ตัวเลข
       input = input.replace(/\D/g, "");
 
-      // เพิ่ม "-" ตามรูปแบบ
       let formattedInput = input.replace(/^(\d{1})(\d{4})(\d{5})(\d{2})(\d{1})$/, "$1-$2-$3-$4-$5");
 
-      // อัพเดตค่าใน input
       event.target.value = formattedInput;
     }
