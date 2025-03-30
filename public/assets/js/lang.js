3afaa9c2bb30d66deeb86ac8c187d1b120336dfa
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2025-03-30 10:24:55 +0700

    Create lang.js

---
+++ public/assets/js/lang.js
@@ -0,0 +1,11 @@
+document.querySelector("[data-th]").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.querySelector("[data-en]").addEventListener("click", function() {
+    window.location.href = "en.html";
+});
+
+document.querySelector("[data-cn]").addEventListener("click", function() {
+    window.location.href = "cn.html";
+});
