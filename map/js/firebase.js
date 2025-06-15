eb4468763d205e9e3810b5d785b4f054edecccba
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2025-06-15 20:43:04 +0700

    Update firebase.js

--- map/js/firebase.js
+++ map/js/firebase.js
@@ -1,12 +1,14 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAklgfina5Ikbx7fXhlDGvsutEKjibDsf8",
+    authDomain: "my-place-map-app.firebaseapp.com",
+    projectId: "my-place-map-app",
+    storageBucket: "my-place-map-app.firebasestorage.app",
+    messagingSenderId: "237185540673",
+    appId: "1:237185540673:web:9c9242ab098389d00f5cc0",
+    measurementId: "G-4ZQ2K9F0NF"
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
