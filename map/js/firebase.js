403646941a53516f5e95f9c0d53a02930aa70914
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2025-06-15 20:06:19 +0700

    Create firebase.js

---
+++ map/js/firebase.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize services
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
